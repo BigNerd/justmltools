1ad312a480bb365e491a5f85086c522f421ddd35
Matthias Burbach <matthias.burbach@web.de>
2020-03-26 16:26:50 +0100

    Provide read access to MLflow run data (params, tags, metrics)

--- justmltools/repo/mlflow_repo_downloader.py
+++ justmltools/repo/mlflow_repo_downloader.py
@@ -1,8 +1,8 @@
 import mlflow.tracking as ml
 from mlflow.tracking.client import MlflowClient
-from mlflow.entities import Experiment, RunInfo, ViewType
+from mlflow.entities import Experiment, Run, RunData, RunInfo, ViewType
 from functools import lru_cache
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 from justmltools.config.local_data_path_config import LocalDataPathConfig
 from justmltools.config.mlflow_data_path_config import MlflowDataPathConfig
@@ -22,20 +22,41 @@ class MlflowRepoDownloader(AbstractRepoDownloader):
         super().__init__(local_data_path_config=local_data_path_config, remote_data_path_config=remote_data_path_config)
         self.__aws_credentials = aws_credentials
         self.__experiment_name = experiment_name
-        self.__run_id = self.__resolve_run_id(run_id)
+        self.__run_id = run_id
+        self.__resolved_run_id = None
 
     @property
     def resolved_run_id(self):
-        return self.__run_id
+        if self.__resolved_run_id is None:
+            self.__resolved_run_id = self.__resolve_run_id(self.__run_id)
+        return self.__resolved_run_id
+
+    def find_or_download_run_metrics(self) -> Dict[str, Any]:
+        run_data: RunData = self.__get_run_data()
+        return run_data.metrics
+
+    def find_or_download_run_params(self) -> Dict[str, Any]:
+        run_data: RunData = self.__get_run_data()
+        return run_data.params
+
+    def find_or_download_run_tags(self) -> Dict[str, Any]:
+        run_data: RunData = self.__get_run_data()
+        return run_data.tags
 
     def _download_object(self, remote_path: str, target_path: str):
         client: MlflowClient = self.__get_mlflow_client()
         dst_path: str = self._local_data_path_config.get_prefix()
         client.download_artifacts(
-            run_id=self.__run_id,
+            run_id=self.resolved_run_id,
             path=remote_path,
             dst_path=dst_path
         )
+
+    @lru_cache(maxsize=1)
+    def __get_run_data(self) -> RunData:
+        client: MlflowClient = self.__get_mlflow_client()
+        run_data: RunData = client.get_run(run_id=self.resolved_run_id)
+        return run_data
 
     def __resolve_run_id(self, run_id: str) -> str:
         """ maps the special run_id "latest" to a concrete id, returns all others as is
--- tests/repo/test_mlflow_repo_downloader.py
+++ tests/repo/test_mlflow_repo_downloader.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
+from mlflow.entities import RunData, Param
+from typing import Any, Dict
 
 from justmltools.repo.mlflow_repo_downloader import MlflowRepoDownloader
 from justmltools.config.local_data_path_config import LocalDataPathConfig
@@ -8,11 +10,26 @@ from justmltools.config.mlflow_data_path_config import MlflowDataPathConfig
 
 class TestMlflowRepoDownloader(TestCase):
 
-    def test_find_or_download_input_config_object(self):
-        _ = MlflowRepoDownloader(
+    def create_downloader(self, run_id: str) -> MlflowRepoDownloader:
+        downloader: MlflowRepoDownloader = MlflowRepoDownloader(
             local_data_path_config=LocalDataPathConfig(prefix="my_local_test_prefix"),
             remote_data_path_config=MlflowDataPathConfig(),
             aws_credentials=Mock(),
             experiment_name="my_test_experiment",
-            run_id="my_test_run_id"
+            run_id=run_id
         )
+        return downloader
+
+    def test_find_or_download_input_config_object(self):
+        run_id: str = "my_test_run_id"
+        sut: MlflowRepoDownloader = self.create_downloader(run_id)
+        self.assertEqual(run_id, sut.resolved_run_id)
+
+    def test_find_or_download_params(self):
+        run_id: str = "my_test_run_id"
+        sut: MlflowRepoDownloader = self.create_downloader(run_id)
+        params = [ Param(key="test_param", value="test_param_value") ]
+        run_data: RunData = RunData(metrics=None, params=params, tags=None)
+        with patch.object(sut, '_MlflowRepoDownloader__get_run_data', return_value=run_data) as method:
+            run_params: Dict[str, Any] = sut.find_or_download_run_params()
+            self.assertEqual("test_param_value", run_params["test_param"])
