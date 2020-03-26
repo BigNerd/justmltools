868c77f74748f1cb4a4a3dc02f21554fee9f6cc8
Matthias Burbach <matthias.burbach@web.de>
2020-03-26 16:26:16 +0100

    Track artifact directories as zip files

--- justmltools/experiment/tracker.py
+++ justmltools/experiment/tracker.py
@@ -1,6 +1,6 @@
 import mlflow
-
 import os
+import shutil
 
 from typing import Any, Dict, List, Optional
 from os.path import basename, sep
@@ -21,12 +21,7 @@ class Tracker:
               metrics: Optional[Dict[str, Any]] = None,
               tags: Optional[Dict[str, str]] = None):
         if artifact_file_paths:
-            for artifact_file_path in artifact_file_paths:
-                artifact_path = artifact_file_path[len(self.__artifact_root_path):-len(basename(artifact_file_path))]
-                artifact_path = artifact_path.strip(sep)  # remove leading and trailing / or \
-                if self.__mlflow_repo_is_remote():
-                    artifact_path = artifact_path.replace(sep, "/")  # replace Windows separator with S3 forward slash
-                mlflow.log_artifact(local_path=artifact_file_path, artifact_path=artifact_path)
+            self.__track_artifacts(artifact_file_paths=artifact_file_paths)
         if parameters:
             for parameter_name, parameter_value in parameters.items():
                 mlflow.log_param(parameter_name, parameter_value)
@@ -36,6 +31,25 @@ class Tracker:
         if tags:
             for tag_name, tag_value in tags.items():
                 mlflow.set_tag(tag_name, tag_value)
+
+    def __track_artifacts(self, artifact_file_paths: List[str]):
+        for artifact_file_path in artifact_file_paths:
+            if os.path.isdir(artifact_file_path):
+                artifact_file_path = self.__zip_dir(dir_path=artifact_file_path)
+            self.__track_artifact_file(artifact_file_path)
+
+    def __track_artifact_file(self, artifact_file_path: str):
+        mlflow_artifact_path: str = \
+            artifact_file_path[len(self.__artifact_root_path):-len(basename(artifact_file_path))]
+        mlflow_artifact_path = mlflow_artifact_path.strip(sep)  # remove leading and trailing / or \
+        if self.__mlflow_repo_is_remote():
+            mlflow_artifact_path = mlflow_artifact_path.replace(sep, "/")  # replace Windows separator with S3 slash
+        mlflow.log_artifact(local_path=artifact_file_path, artifact_path=mlflow_artifact_path)
+
+    @staticmethod
+    def __zip_dir(dir_path: str) -> str:
+        shutil.make_archive(base_name=dir_path, format="zip", root_dir=dir_path)
+        return dir_path + ".zip"
 
     @staticmethod
     def end_run():
--- tests/experiment/test_tracker.py
+++ tests/experiment/test_tracker.py
@@ -4,6 +4,14 @@ from unittest.mock import MagicMock, patch
 from justmltools.experiment.tracker import Tracker
 
 
+def mocked_os_path_isdir(path: str) -> bool:
+    return "dir" in path
+
+
+def mocked_make_archive(base_name: str, format: str, root_dir: str):
+    pass
+
+
 def mocked_mlflow_set_experiment(experiment_name: str):
     pass
 
@@ -30,13 +38,17 @@ def mocked_mlflow_end_run():
 
 class TestTracker(TestCase):
 
-    @patch('mlflow.set_experiment', side_effect=mocked_mlflow_set_experiment)
-    @patch('mlflow.log_artifact', side_effect=mocked_mlflow_log_artifact)
-    @patch('mlflow.log_param', side_effect=mocked_mlflow_log_param)
-    @patch('mlflow.log_metric', side_effect=mocked_mlflow_log_metric)
-    @patch('mlflow.set_tag', side_effect=mocked_mlflow_set_tag)
     @patch('mlflow.end_run', side_effect=mocked_mlflow_end_run)
+    @patch('mlflow.set_tag', side_effect=mocked_mlflow_set_tag)
+    @patch('mlflow.log_metric', side_effect=mocked_mlflow_log_metric)
+    @patch('mlflow.log_param', side_effect=mocked_mlflow_log_param)
+    @patch('mlflow.log_artifact', side_effect=mocked_mlflow_log_artifact)
+    @patch('mlflow.set_experiment', side_effect=mocked_mlflow_set_experiment)
+    @patch('shutil.make_archive', side_effect=mocked_make_archive)
+    @patch('os.path.isdir', side_effect=mocked_os_path_isdir)
     def test_(self,
+              os_path_isdir_mock: MagicMock,
+              shutil_make_archive_mock: MagicMock,
               mlflow_set_experiment_mock: MagicMock,
               mlflow_log_artifact_mock: MagicMock,
               mlflow_log_param_mock: MagicMock,
@@ -45,11 +57,29 @@ class TestTracker(TestCase):
               mlflow_end_run_mock: MagicMock):
         self.sut: Tracker = Tracker(experiment_name="my_test_experiment", artifact_root_path="path_to_my_artifacts")
         self.sut.start_run()
+        mlflow_set_experiment_mock.assert_called_once()
         # perform the actual experiment collecting data to track... and then data over to the tracker
         self.sut.track(
             artifact_file_paths=["my_artifact_path_1", "my_artifact_path_2"],
+            parameters={"my_param": "my_param_value"},
+            metrics={"my_metric": 1.0},
+            tags={"my_tag": "my_tag_value"}
+        )
+        mlflow_log_param_mock.assert_called()
+        mlflow_log_metric_mock.assert_called()
+        mlflow_set_tag_mock.assert_called()
+        mlflow_log_artifact_mock.assert_called()
+        os_path_isdir_mock.assert_called()
+        shutil_make_archive_mock.assert_not_called()
+
+        self.sut.track(
+            artifact_file_paths=["my_artifact_path_1", "my_artifact_path_2", "my_artifact_dir_path"],
             parameters={},
             metrics={},
             tags={}
         )
+        os_path_isdir_mock.assert_called()
+        shutil_make_archive_mock.assert_called_once()
+
         self.sut.end_run()
+        mlflow_end_run_mock.assert_called_once()
