4a0a5daa6a6c1650dc363f6152f969b1e979e947
Matthias Burbach <matthias.burbach@web.de>
2020-12-21 17:49:46 +0100

    Simply mock setup

--- tests/s3/test_s3_bucket_object_downloader.py
+++ tests/s3/test_s3_bucket_object_downloader.py
@@ -5,26 +5,17 @@ from justmltools.s3.s3_bucket_object_downloader import S3BucketObjectDownloader
 from justmltools.s3.aws_credentials import AwsCredentials
 
 
-def mocked_os_path_exists(path: str) -> bool:
-    return True
-
-
-def mocked_os_path_isdir(path: str) -> bool:
-    return True
-
-
-def mocked_os_path_isfile(path: str) -> bool:
-    return True
-
-
-def mocked_boto3_resource(resource_name: str, aws_access_key_id=None, aws_secret_access_key=None, region_name=None):
-    return MagicMock()
-
-
+@patch('os.path.isdir', return_value=True)
+@patch('os.path.isfile', return_value=True)
+@patch('boto3.resource')
 class TestS3BucketObjectDownloader(TestCase):
 
-    @patch('boto3.resource', side_effect=mocked_boto3_resource)
-    def test_init(self, boto3_resource: MagicMock):
+    def test_init(
+            self,
+            boto3_resource: MagicMock,
+            os_path_is_file: MagicMock,
+            os_path_is_dir: MagicMock
+    ):
         sut = S3BucketObjectDownloader()
         sut = S3BucketObjectDownloader(
             credentials=AwsCredentials(
@@ -35,12 +26,12 @@ class TestS3BucketObjectDownloader(TestCase):
         )
         self.assertEqual(2, boto3_resource.call_count)
 
-    @patch('os.path.isfile', side_effect=mocked_os_path_isfile)
-    @patch('boto3.resource', side_effect=mocked_boto3_resource)
     def test_download_with_no_override(
             self,
             boto3_resource: MagicMock,
-            os_path_isfile_function: MagicMock):
+            os_path_is_file: MagicMock,
+            os_path_is_dir: MagicMock
+    ):
         sut = S3BucketObjectDownloader()
         self.assertEqual(1, boto3_resource.call_count)
         with self.assertRaises(FileExistsError):
@@ -51,14 +42,14 @@ class TestS3BucketObjectDownloader(TestCase):
                 override_existing_file=False
             )
         self.assertEqual(1, boto3_resource.call_count)
-        self.assertEqual(1, os_path_isfile_function.call_count)
+        self.assertEqual(1, os_path_is_file.call_count)
 
-    @patch('os.path.isfile', side_effect=mocked_os_path_isfile)
-    @patch('boto3.resource', side_effect=mocked_boto3_resource)
     def test_download_with_override(
             self,
             boto3_resource: MagicMock,
-            os_path_isfile_function: MagicMock):
+            os_path_is_file: MagicMock,
+            os_path_is_dir: MagicMock
+    ):
         sut = S3BucketObjectDownloader()
         self.assertEqual(1, boto3_resource.call_count)
         sut.download(
@@ -68,16 +59,14 @@ class TestS3BucketObjectDownloader(TestCase):
             override_existing_file=True
         )
         self.assertEqual(1, boto3_resource.call_count)
-        self.assertEqual(0, os_path_isfile_function.call_count)
+        self.assertEqual(0, os_path_is_file.call_count)
 
-    @patch('os.path.isdir', side_effect=mocked_os_path_isdir)
-    @patch('os.path.isfile', side_effect=mocked_os_path_isfile)
-    @patch('boto3.resource', side_effect=mocked_boto3_resource)
     def test_download_dir_with_override(
             self,
             boto3_resource: MagicMock,
-            os_path_isfile_function: MagicMock,
-            os_path_isdir_function: MagicMock):
+            os_path_is_file: MagicMock,
+            os_path_is_dir: MagicMock
+    ):
         sut = S3BucketObjectDownloader()
         self.assertEqual(1, boto3_resource.call_count)
         sut.download(
@@ -87,5 +76,5 @@ class TestS3BucketObjectDownloader(TestCase):
             override_existing_file=True
         )
         self.assertEqual(1, boto3_resource.call_count)
-        self.assertEqual(0, os_path_isfile_function.call_count)
-        self.assertEqual(1, os_path_isdir_function.call_count)
+        self.assertEqual(0, os_path_is_file.call_count)
+        self.assertEqual(1, os_path_is_dir.call_count)
